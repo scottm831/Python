ff97b08e9b26cf715956340f9e23ba3dd3318bc1
Scott Milne <scottmilne831@gmail.com>
2021-11-12 11:34:15 -0700

    added python

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+print("Hello World")
+
+print("    /|")
+print("   / |")
+print("  /  |")
+print(" /___|")
+
+character_name = "Jon"
+character_age = 35
+is_male = True
+print("there once was a dude named "+character_name+",")
+print("he didn't really try a bong")
+print("he was "+character_age+",")
+print("he lived in a dive")
+print("but his equity increased times five") 
